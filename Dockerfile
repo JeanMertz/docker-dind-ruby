286c8f4c2245197400157c65d1a21674a6b12292
Jean Mertz <jean@mertz.fm>
2015-05-31 23:54:34 +0200

    add missing package dependencies

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ ENTRYPOINT ["wrapdocker"]
 
 VOLUME /var/lib/docker
 
-RUN apk-install iptables ca-certificates lxc e2fsprogs wget docker device-mapper
+RUN apk-install iptables ca-certificates lxc e2fsprogs wget docker socat perl \
+                device-mapper coreutils
+
 RUN wget -qO- https://get.docker.com/builds/Linux/x86_64/docker-1.6.2 > /usr/bin/docker
 RUN wget -qO- https://raw.githubusercontent.com/jpetazzo/dind/master/wrapdocker > /usr/local/bin/wrapdocker
 RUN chmod +x /usr/bin/docker /usr/local/bin/wrapdocker
--- spec/dind-ruby_spec.rb
+++ spec/dind-ruby_spec.rb
@@ -19,6 +19,20 @@ describe RSpec.configuration.docker_image_name do
     its(:exit_status) { is_expected.to eq 0 }
   end
 
+  context 'Drone CI requirements' do
+    describe command('command -v socat') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+
+    describe command('command -v perl') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+
+    describe command('command -v nproc') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+  end
+
   describe command('docker version') do
     its(:stdout) { is_expected.to include('Client version: 1.6.2') }
     its(:stdout) { is_expected.to include('Server version: 1.6.2') }
